dc14de244dcb505d9277a8c238168dfb96178646
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-09-16 22:31:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
+
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-	char target = s2.charCodeAt(0);
-	for(let i=0;i<s1.length;i++)
-		{
-			if(target===str.charCodeAt(i))
-			{
-				return i;
-			}
-		}
-	return -1;	
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerS1 = s1.toLowerCase();
+  const lowerS2 = s2.toLowerCase();
+
+  // Use indexOf to find the position of s2 in s1
+  return lowerS1.indexOf(lowerS2);
 }
+
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
+
