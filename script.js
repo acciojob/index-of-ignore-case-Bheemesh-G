3658b86127cb21a884f925dc3a2c29de45eacc24
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-09-16 21:48:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	const str = s1.toLowerCase();
+	const sunStr = s2.toLowerCase();
+	const char = s2.charAt(0);
+	for(let i=0;i<str.length;i++)
+		{
+			
+			if(char==str.charAt(i))
+			{
+				return i;
+			}
+		}
+	return -1;	
 }
-
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
